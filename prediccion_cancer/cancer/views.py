11840a747949d0368627d32872668fd72925bc95
alberto <albertomr317@gmail.com>
2025-04-19 18:40:22 +0200

    añadido select imagen

--- prediccion_cancer/cancer/views.py
+++ prediccion_cancer/cancer/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ImagenForm
 
 # Create your views here.
 
@@ -7,3 +8,15 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "index.html")
 
+def subir_imagen(request):
+    if request.method == 'POST' and request.FILES['imagen']:
+        form = ImagenForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Aquí puedes manejar la imagen, guardarla o procesarla
+            imagen = form.cleaned_data['imagen']
+            # Guardar o hacer algo con la imagen
+            # ejemplo: imagen.save()
+    else:
+        form = ImagenForm()
+    
+    return render(request, 'subir_imagen.html', {'form': form})
