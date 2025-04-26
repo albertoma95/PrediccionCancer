7b647b5b1485a45239a5b26d59f1cff699c5b086
bocho <albertomr317@gmail.com>
2025-04-26 10:05:57 +0200

    unificado modelos xgb y cnn

--- prediccion_cancer/cancer/static/js/scripts.js
+++ prediccion_cancer/cancer/static/js/scripts.js
@@ -1,67 +1,87 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const formulario = document.getElementById('formulario');
+  
+  formulario.addEventListener('submit', function(e) {
+    e.preventDefault();  // Prevenir el envío normal del formulario
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const formulario = document.getElementById('formulario');
-    
-    formulario.addEventListener('submit', function(e) {
-      e.preventDefault();  // Prevenir el envío normal del formulario
+    // Crear un objeto FormData para recoger todos los campos, incluyendo la imagen
+    const formData = new FormData(formulario);
 
+    // Obtener el archivo de la imagen
+    const imagen = document.getElementById('id_imagen').files[0];
 
-      // Recoger los datos de los campos de texto y checkbox
-      const datos = {
-        Sexo: document.getElementById('Sexo').value,  // Recoger el valor del select
-        Age: document.getElementById('Age').value,  // Recoger el valor de la edad
-        'Family history': document.getElementById('Family history').checked? 'Yes' : 'No'? 'Yes' : 'No',  // Recoger el valor de Family history
-        smoke: document.getElementById('smoke').checked? 'Yes' : 'No',  // Recoger el estado del checkbox (true o false)
-        alcohol: document.getElementById('alcohol').checked? 'Yes' : 'No',  // Recoger el estado del checkbox (true o false)
-        obesity: document.getElementById('obesity').value,  // Recoger el valor del select de obesidad
-        diet: document.getElementById('diet').value,  // Recoger el valor de diet
-        Screening_History: document.getElementById('Screening_History').value,  // Recoger el valor de Screening_History
-        Healthcare_Access: document.getElementById('Healthcare_Access').value,  // Recoger el valor de Healthcare_Access
-        cancer_stage: document.getElementById('cancer_stage').value,  // Recoger el valor de cancer_stage
-        tumor_size: document.getElementById('tumor_size').value,  // Recoger el valor de tumor_size
-        early_detection: document.getElementById('early_detection').checked? 'Yes' : 'No',  // Recoger el valor de early_detection
-        inflammatory_bowel_disease: document.getElementById('inflammatory_bowel_disease').checked? 'Yes' : 'No',  // Recoger el estado del checkbox
-        relapse: document.getElementById('relapse').checked? 'Yes' : 'No'
-      };
+    // Si hay una imagen, convertirla a base64
+    if (imagen) {
+      const reader = new FileReader();
+      reader.onloadend = function() {
+        // Una vez que la imagen se ha convertido a base64, recoger el valor
+        const imagenBase64 = reader.result;
 
+        // Recoger el resto de los datos del formulario
+        const datos = {
+          Sexo: document.getElementById('Sexo').value,
+          Age: document.getElementById('Age').value,
+          'Family history': document.getElementById('Family history').checked ? 'Yes' : 'No',
+          smoke: document.getElementById('smoke').checked ? 'Yes' : 'No',
+          alcohol: document.getElementById('alcohol').checked ? 'Yes' : 'No',
+          obesity: document.getElementById('obesity').value,
+          diet: document.getElementById('diet').value,
+          Screening_History: document.getElementById('Screening_History').value,
+          Healthcare_Access: document.getElementById('Healthcare_Access').value,
+          cancer_stage: document.getElementById('cancer_stage').value,
+          tumor_size: document.getElementById('tumor_size').value,
+          early_detection: document.getElementById('early_detection').checked ? 'Yes' : 'No',
+          inflammatory_bowel_disease: document.getElementById('inflammatory_bowel_disease').checked ? 'Yes' : 'No',
+          relapse: document.getElementById('relapse').checked ? 'Yes' : 'No',
+          imagen_base64: imagenBase64  // Añadimos la imagen convertida a base64
+        };
 
-      // Usar fetch para enviar los datos al servidor
-      fetch('/predict/', {  // La URL de la vista
-        method: 'POST',
-        headers: {
-          "X-CSRFToken": getCookie("csrftoken"),
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(datos),
-      })
-      .then(response => response.json())  // Convertimos la respuesta en JSON
-      .then(data => {
-        if (data.prediction) {
-          // Si la predicción se recibió, mostrarla
-          document.getElementById('prediccion').textContent = data.prediction;
-          document.getElementById('resultados').style.display = 'block';
-        } else {
-          alert('Error en la predicción.');
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        alert('Hubo un error al procesar la solicitud.');
-      });
-    });
-
-    function getCookie(name) {
-      let cookieValue = null;
-      if (document.cookie && document.cookie !== "") {
-          const cookies = document.cookie.split(";");
-          for (let i = 0; i < cookies.length; i++) {
-              const cookie = cookies[i].trim();
-              if (cookie.substring(0, name.length + 1) === name + "=") {
-                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                  break;
-              }
+        // Usar fetch para enviar los datos al servidor
+        fetch('/predict/', {  // La URL de la vista
+          method: 'POST',
+          headers: {
+            "X-CSRFToken": getCookie("csrftoken"),
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(datos),  // Enviamos los datos como JSON
+        })
+        .then(response => response.json())  // Convertimos la respuesta en JSON
+        .then(data => {
+          // Verificamos si la predicción es 1 (maligno) o 0 (benigno)
+          if (data.prediction === 1) {
+            // Si la predicción es 1, mostrar "Cáncer Maligno"
+            document.getElementById('prediccion').textContent = "Cáncer Maligno";
+          } else if (data.prediction === 0) {
+            // Si la predicción es 0, mostrar "Cáncer Benigno"
+            document.getElementById('prediccion').textContent = "Cáncer Benigno";
+          } else {
+            alert('Error en la predicción.');
           }
-      }
-      return cookieValue;
+          document.getElementById('resultados').style.display = 'block';
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          alert('Hubo un error al procesar la solicitud.');
+        });
+      };
+      reader.readAsDataURL(imagen);  // Convertir la imagen a base64
+    } else {
+      alert("Por favor, sube una imagen.");
     }
   });
+
+  function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === name + "=") {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+  }
+});
--- prediccion_cancer/cancer/views.py
+++ prediccion_cancer/cancer/views.py
@@ -2,8 +2,12 @@ from django.http import JsonResponse
 from django.shortcuts import render
 import pandas as pd
 import joblib
-from django.core.files.storage import FileSystemStorage
+from django.core.files.storage import default_storage
 import json
+from tensorflow.keras.models import load_model
+import cv2
+import base64
+import numpy as np
 
 def index(request):
     return render(request, "index.html")
@@ -12,38 +16,65 @@ def index(request):
 
 def predict(request):
     if request.method == 'POST':  # Verificar si es una petición AJAX
-            body = json.loads(request.body)
+        body = json.loads(request.body)
 
-            data = {
-                'Sexo': [body.get('Sexo')],
-                'Age': [float(body.get('Age'))],
-                'Family history': [body.get('Family history')],
-                'smoke': [body.get('smoke')],
-                'alcohol': [body.get('alcohol')],
-                'obesity': [body.get('obesity')],
-                'diet': [body.get('diet')],
-                'Screening_History': [body.get('Screening_History')],
-                'Healthcare_Access': [body.get('Healthcare_Access')],
-                'cancer_stage': [body.get('cancer_stage')],
-                'tumor_size': [float(body.get('tumor_size'))],
-                'early_detection': [body.get('early_detection')],
-                'inflammatory_bowel_disease': [body.get('inflammatory_bowel_disease')],
-                'relapse': [body.get('relapse')],
-            }
+        # Obtener la imagen en base64 desde el JSON
+        imagen_base64 = body.get('imagen_base64')
+        if not imagen_base64:
+            return JsonResponse({'error': 'No se ha recibido ninguna imagen'}, status=400)
 
-            print(data)
-            
-            # Convertir el diccionario en un DataFrame de pandas
-            df = pd.DataFrame(data)
+        # Decodificar la imagen en base64
+        img_data = base64.b64decode(imagen_base64.split(',')[1])  # Eliminar el prefijo 'data:image/jpeg;base64,' si existe
+        nparr = np.frombuffer(img_data, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (128, 128))  # Redimensionar la imagen a 128x128
+        img = img / 255.0  # Normalizar la imagen
+        img = img.reshape(1, 128, 128, 3)  # Ajustar la forma de la imagen para el modelo
 
-            # Cargar el modelo entrenado (asegúrate de que esté en la ruta correcta)
-            model = joblib.load('cancer/models/xgb_model.pkl')
+        # Cargar el modelo de imágenes entrenado
+        model_image = load_model('cancer/models/cnn_model.h5')
 
-            # Realizar la predicción usando el modelo
-            prediction = model.predict(df)
+        # Realizar la predicción usando el modelo de imágenes
+        prediction_image = model_image.predict(img)
+        prediction_image = int(prediction_image[0] > 0.5)  # Convertir a 0 o 1
 
-            # Devolver la predicción en formato JSON para AJAX
-            return JsonResponse({'prediction': int(prediction[0])})
+        # Preparar los datos para el modelo tabular
+        data = {
+            'Sexo': [body.get('Sexo')],
+            'Age': [float(body.get('Age'))],
+            'Family history': [body.get('Family history')],
+            'smoke': [body.get('smoke')],
+            'alcohol': [body.get('alcohol')],
+            'obesity': [body.get('obesity')],
+            'diet': [body.get('diet')],
+            'Screening_History': [body.get('Screening_History')],
+            'Healthcare_Access': [body.get('Healthcare_Access')],
+            'cancer_stage': [body.get('cancer_stage')],
+            'tumor_size': [float(body.get('tumor_size'))],
+            'early_detection': [body.get('early_detection')],
+            'inflammatory_bowel_disease': [body.get('inflammatory_bowel_disease')],
+            'relapse': [body.get('relapse')],
+        }
+
+        # Convertir el diccionario en un DataFrame de pandas
+        df = pd.DataFrame(data)
+
+        # Cargar el modelo tabular entrenado
+        model_tabular = joblib.load('cancer/models/xgb_model.pkl')
+
+        # Realizar la predicción usando el modelo tabular
+        prediction_tabular = model_tabular.predict(df)
+        prediction_tabular = int(prediction_tabular[0])  # Asegúrate de obtener el valor entero
+
+        # Hacer una media ponderada entre las predicciones
+        weight_tabular = 0.4
+        weight_image = 0.6
+
+        final_prediction = (prediction_tabular * weight_tabular) + (prediction_image * weight_image)
+        final_prediction = 1 if final_prediction >= 0.5 else 0  # Si la media ponderada es mayor o igual a 0.5, consideramos cáncer maligno
+
+        # Devolver la predicción en formato JSON para AJAX
+        return JsonResponse({'prediction': final_prediction})
         
     return JsonResponse({'error': 'Petición no válida'}, status=400)
 
